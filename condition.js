345650636490f13fd80c571b8cb5bf9a01c8656c
solja2008 <solzhi91@gmail.com>
2023-03-02 00:06:51 +0800

    index.html

---
+++ condition.js
@@ -0,0 +1,10 @@
+let game = prompt('would you like to play the game?');
+
+
+    if (game = yes) {
+                alert('sure, you will like it');
+                }
+    else if (game = not) {
+                alert('never say not')
+    }
+    
