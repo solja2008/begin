34eb5a131828c652ac84518b835de45cd36f366b
solja2008 <solzhi91@gmail.com>
2023-03-02 00:37:38 +0800

    index.html

--- condition.js
+++ condition.js
@@ -1,9 +1,13 @@
 let game = prompt('would you like to play the game?');
 
-    if (game == yes) {
+    if (game == 'yes')   {
                 alert('sure, you will like it');
                 }
-    else if (game == no) {
-                alert('never say no')
-    }
+    else if (game=='no') {
+                    alert('never say no')
+                }
+    else                 { 
+                alert('you are bad mazafaka =)')
+                }
+   
     
