cb0491b5cabdcb7e218c08d479962d00735be9e0
solja2008 <solzhi91@gmail.com>
2023-03-07 19:13:07 +0800

    condition.js index.html

--- condition.js
+++ condition.js
@@ -9,11 +9,21 @@ let game = prompt('would you like to play the game?');
     else                 { 
                 alert('you are bad mazafaka =)')
                 }
+
 let quiz = prompt('какова популяция земли?');
+
     if (quiz == '8000000000')  {
                      alert ('яблоку негде упасть')
                 }  
     else {
         alert('тебе лучше и не знать!')
     }  
+
+let quiz2 = prompt('кто самый жадный человек на земле?');
+    if (quiz2 =='не знаю') {
+        alert('возможно, он внутри тебя');
+    }
+    else {
+        alert('прикинь, есть такая Генриетта Грин')
+    }
     
